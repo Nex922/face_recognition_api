23e95c07ae0962e6233e981548aeee2ff41d16e1
nex <nex922@gmail.com>
2018-11-30 19:10:55 +0100

    syntax

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ const profile = require('./controllers/profile');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'process.env.DATABASE_HOST',
+    host : process.env.DATABASE_HOST,
     port: '5432',
-    user : 'process.env.DATABASE_USER',
-    password : 'process.env.DATABASE_PASSWORD',
-    database : 'process.env.DATABASE_NAME'
+    user : process.env.DATABASE_USER,
+    password : process.env.DATABASE_PASSWORD,
+    database : process.env.DATABASE_NAME,
   	ssl: true
   }
 });
